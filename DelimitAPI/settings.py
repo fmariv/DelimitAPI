c8887cb203d594d45da2c766517f682d636ff2b3
Fran Martin <fmartinrivas2@gmail.com>
2021-01-12 14:04:14 +0100

    ADDED App doc_generator

--- DelimitAPI/settings.py
+++ DelimitAPI/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'CQline.apps.CqlineConfig'
+    'CQline',
+    'doc_generator'
 ]
 
 MIDDLEWARE = [
---
+++ doc_generator/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DocGeneratorConfig(AppConfig):
+    name = 'doc_generator'
